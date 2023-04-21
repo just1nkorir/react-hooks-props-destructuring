9d8648fa5356d54943c8f59118b77033344eb770
just1nkorir <justin.korir@student.moringaschool.com>
2023-04-21 13:36:50 +0300

    .

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ function MovieCard({
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
 }) {
-  return (
+  return  (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
